1b408cab61ce64c449c1d74a239fd6dd24943bc4
kmusante <kenm@eurekapayments.com>
2017-02-04 18:09:08 -0800

    Add files via upload

--- salesrep.py
+++ salesrep.py
@@ -30,8 +30,6 @@ def showSalesReps():
     return render_template('salesreps.html', salesreps=salesreps)
 
 # Create a new sales rep
-
-
 @app.route('/salesreps/new/', methods=['GET', 'POST'])
 def newSalesReps():
     if request.method == 'POST':
@@ -44,8 +42,6 @@ def newSalesReps():
         return render_template('newSalesReps.html')
 
 # Edit a sales rep
-
-
 @app.route('/salesreps/<int:salesrep_id>/edit/', methods=['GET', 'POST'])
 def editSalesRep(salesrep_id):
     editedSalesRep = session.query(
@@ -73,8 +69,6 @@ def deleteSalesRep(salesrep_id):
         return render_template('deleteSalesRep.html', salesrep=repToDelete)
 
 # Show sales rep details for selected sales rep
-
-
 @app.route('/salesreps/<int:salesrep_id>/')
 @app.route('/salesreps/<int:salesrep_id>/repdetails')
 def showRep(salesrep_id):
@@ -88,46 +82,57 @@ def showRep(salesrep_id):
 @app.route('/salesreps/<int:salesrep_id>/repdetails/add/', methods=['GET', 'POST'])
 def addRepDetails(salesrep_id):
     salesrep = session.query(SalesReps).filter_by(id=salesrep_id).one()
-    if salesrepdetails==session.query(RepDetails).filter_by(salesrep_id=salesrep_id):
-        print "*****************", salesrepdetails, salesrepdetails.payout
-        print salesrep
+    salesrepdetails=session.query(RepDetails).filter_by(salesrep_id=salesrep_id).first()
     if request.method == 'POST':
-    	#this line 93 might be messed up??
         newItem = RepDetails(name=salesrep.name, payout=request.form[
-                           'payout'], sub_reps=request.form['sub_reps'], contractor=request.form['contractor'], salesrep_id=salesrep_id)
+                   'payout'], sub_reps=request.form['sub_reps'], 
+                   contractor=request.form['contractor'], salesrep_id=salesrep_id)
+        #if newItem.payout!=None and newItem.contractor!=None and newItem.salesrep_id!=None:
         session.add(newItem)
         session.commit()
         flash('Rep Details %s Successfully Added' % (newItem.name))
         return redirect(url_for('showRep', salesrep_id=salesrep_id))
+    #else:
+    #	print "Need Rep Details completed to commit changes"
     else:
         return render_template('addRepDetails.html', salesrep_id=salesrep_id, salesrep_name=salesrep.name)
 
 # Edit a rep details
-
-
 @app.route('/salesreps/<int:salesrep_id>/repdetails/edit', methods=['GET', 'POST'])
 def editRepDetails(salesrep_id):
-
-    editedRep = session.query(RepDetails).filter_by(id=salesrep_id).one()
+    #editedRep = session.query(RepDetails).filter_by(id=salesrep_id).first()
     salesrep = session.query(SalesReps).filter_by(id=salesrep_id).one()
+    details=session.query(RepDetails).filter_by(salesrep_id=salesrep_id).all()
+    for i in details:
+        if request.method == 'POST':
+            if request.form['payout']:
+                i.payout = request.form['payout']
+            if request.form['sub_reps']:
+                i.sub_reps = request.form['sub_reps']
+            if request.form['contractor']:
+                i.contractor = request.form['contractor']
+            session.add(salesrep)
+            session.commit()
+            flash('Sales Rep Successfully Edited')
+            return redirect(url_for('showRep', salesrep_id=salesrep_id))
+        return render_template('editRepDetails.html', salesrep_id=salesrep_id,
+    	    salesrep_name=salesrep.name, details_payout=i.payout, details_sub_reps=i.sub_reps,
+    	    details_contractor=i.contractor)
+
+
+# Delete a rep details
+@app.route('/salesreps/<int:salesrep_id>/repdetails/delete', methods=['GET', 'POST'])
+def deleteRepDetails(salesrep_id):
+    salesrep = session.query(SalesReps).filter_by(id=salesrep_id).one()
+    details=session.query(RepDetails).filter_by(salesrep_id=salesrep_id).all()
     if request.method == 'POST':
-        if request.form['name']:
-            editedRep.name = request.form['name']
-        if request.form['payout']:
-            editedRep.payout = request.form['payout']
-        if request.form['sub_reps']:
-            editedRep.sub_reps = request.form['sub_reps']
-        if request.form['contractor']:
-            editedItem.contractor = request.form['contractor']
-        session.add(editedRep)
+        session.delete(details)
+        flash('%s Successfully Deleted Details for' % salesrep.name)
         session.commit()
-        flash('Sales Rep Successfully Edited')
         return redirect(url_for('showRep', salesrep_id=salesrep_id))
     else:
-        return render_template('editSalesReps.html', salesrep_id=salesrep_id, salesrep_name=salesrep.name, name=editedRep)
-
-
-
+        return render_template('deleteRepDetails.html', salesrep_id=salesrep_id, 
+        	salesrep_name=salesrep.name)
 
 
 
